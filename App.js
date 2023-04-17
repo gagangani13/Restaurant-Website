7a7acc33bef6440626fe8104aec7135048bf553c
gagangani13 <127178242+gagangani13@users.noreply.github.com>
2023-04-17 16:39:31 +0530

    Making a hard Coded Cart without any Functionality

--- App.js
+++ App.js
@@ -1,9 +1,20 @@
 import Header from "./Components/Layout/Header";
 import "./mainPage.css"
+import Message from "./Components/Layout/Message";
+import MealsList from "./Components/Layout/MealsList";
+import { useState } from "react";
+import CartPopUp from "./Components/Layout/CartPopUp";
 function App() {
+  const[cartPop,setCartPop]=useState(false)
+  function cartButton() {
+    setCartPop(true)
+  }
   return (
     <div>
-      <Header/> 
+      <Header onApp={cartButton}/> 
+      <Message/>
+      <MealsList/>
+      {cartPop&&<CartPopUp/>}
     </div>
   );
 }
--- Header.js
+++ Header.js
@@ -2,21 +2,21 @@ import React from 'react'
 import "./Header.css"
 import meals from "../../meals2.jpg"
 import CartButton from "./CartButton"
-const Header = () => {
+const Header = (props) => {
   return (
-    <>
+    <div>
     <header>
         <h2>
             <i class="fa-solid fa-salad fa-fade"></i>
             South Indian Meals
         </h2>
-        <CartButton/>
+        <CartButton onHeader={props.onApp}/>
     </header>
     <div className='main-image'>
          <img className="main-image img" src={meals} alt='meals'/>
     </div>
    
-    </>
+    </div>
     
   )
 }
